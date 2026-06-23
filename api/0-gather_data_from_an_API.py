dc53c80584a00d87bfa8cfd09695a8cf0fe7f6d2
emmanuel <e.rangira@alustudent.com>
2026-06-24 03:58:24 +1200

    new gather file

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-"""Gather data from a REST API for a given employee's TODO progress."""
-import requests
+"""
+Script that, for a given employee ID, returns information
+about his/her TODO list progress using a REST API.
+"""
 import sys
-if __name__ == "__main__":
-    employee_id = sys.argv[1]
+import requests
 
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
     base_url = "https://jsonplaceholder.typicode.com"
 
-    user = requests.get("{}/users/{}".format(base_url, employee_id)).json()
-    todos = requests.get(
-        "{}/todos".format(base_url), params={"userId": employee_id}
-    ).json()
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    todos_url = "{}/todos".format(base_url)
 
-    employee_name = user.get("name")
-    total_tasks = len(todos)
-    done_tasks = [task for task in todos if task.get("completed")]
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
+
+    todos_response = requests.get(todos_url, params={"userId": employee_id})
+    todos_data = todos_response.json()
+
+    total_tasks = len(todos_data)
+    done_tasks = [task for task in todos_data if task.get("completed")]
     number_of_done_tasks = len(done_tasks)
 
     print("Employee {} is done with tasks({}/{}):".format(
