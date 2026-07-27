61fcbf3b8902c7d3f4d1e863556aacdc713eddc2
emmanuel <e.rangira@alustudent.com>
2026-07-27 15:28:11 +0200

    New Readme file

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
+"""Fetch and display a given employee's TODO list progress.
+
+Uses the JSONPlaceholder REST API. Takes an employee ID as a command-line
+argument and prints how many of that employee's tasks are completed,
+followed by the title of each completed task.
 """
-Script that, for a given employee ID, returns information
-about his/her TODO list progress using a REST API.
-"""
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
+    employee_id = sys.argv[1]
     base_url = "https://jsonplaceholder.typicode.com"
 
-    user_url = "{}/users/{}".format(base_url, employee_id)
-    todos_url = "{}/todos".format(base_url)
+    user = requests.get("{}/users/{}".format(base_url, employee_id)).json()
+    todos = requests.get(
+        "{}/todos".format(base_url), params={"userId": employee_id}
+    ).json()
 
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
-
-    todos_response = requests.get(todos_url, params={"userId": employee_id})
-    todos_data = todos_response.json()
-
-    total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task.get("completed")]
-    number_of_done_tasks = len(done_tasks)
+    employee_name = user.get("name")
+    completed_tasks = [t for t in todos if t.get("completed") is True]
+    total_tasks = len(todos)
 
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, number_of_done_tasks, total_tasks))
+        employee_name, len(completed_tasks), total_tasks))
 
-    for task in done_tasks:
+    for task in completed_tasks:
         print("\t {}".format(task.get("title")))
